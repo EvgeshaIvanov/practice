c299261d4fe61e79c30d077a7b441b930b4b0191
Eugene Ivanov <most1360@yandex.ru>
2022-06-30 18:00:40 +0300

    Commit#1

---
+++ src/main/kotlin/hyperskill/UniqueCharactersTask .kt
@@ -0,0 +1,20 @@
+package hyperskill
+
+/*
+Write a program that reads a word and prints the number of characters that appear in it only once.
+Sample Input 1:
+abracadabra
+Sample Output 1:
+2
+ */
+fun main() {
+
+    val str = readln()
+    var count = 0
+    str.forEach { if (!str.substringAfter(it).contains(it)) count++ }
+
+    for (el in str) {
+        if (!str.substringAfter(el).contains(el)) count++
+    }
+    println(count)
+}
