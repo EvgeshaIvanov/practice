7c828826fe78772260ab8b3577538f8f7b19300a
Eugene Ivanov <most1360@yandex.ru>
2022-07-01 18:21:50 +0300

    Commit#2

---
+++ src/main/kotlin/hyperskill/makingCoffee/EstimateTheNumberOfServingsStage3_6.kt
@@ -0,0 +1,45 @@
+package hyperskill.makingCoffee
+
+
+fun main() {
+    print("Write how many ml of water the coffee machine has: ")
+    val water = readln().toInt()
+    print("Write how many ml of milk  the coffee machine has: ")
+    val milk = readln().toInt()
+    print("Write how many grams of coffee beans the coffee machine has: ")
+    val beans = readln().toInt()
+    print("Write how many cups of coffee you will need: ")
+    val cups = readln().toInt()
+    val totalCoffee = totalCount(water, milk, beans)
+    if (!validate(water, milk, beans)) {
+        if (cups == 0) {
+            println("Yes, I can make that amount of coffee ")
+        } else
+            println("No, I can make only 0 cups of coffee")
+    } else {
+        if (totalCoffee - cups < 0) {
+            println("No, I can make only $totalCoffee cups of coffee")
+        } else if (cups == totalCoffee) {
+            println("Yes, I can make that amount of coffee")
+        } else {
+            println(
+                "Yes, I can make that amount of coffee (and even $totalCoffee more than that)"
+            )
+        }
+    }
+}
+
+fun totalCount(water: Int, milk: Int, beans: Int): Int {
+
+    val minCounter = minOf(water / 200, milk / 50, beans / 15)
+
+    if (milk / 50 >= minCounter && beans / 15 >= minCounter && water / 200 >= minCounter) {
+        return minCounter
+    }
+    return 0
+}
+
+
+fun validate(water: Int, milk: Int, beans: Int): Boolean {
+    return water >= 200 && milk >= 50 && beans >= 15
+}
