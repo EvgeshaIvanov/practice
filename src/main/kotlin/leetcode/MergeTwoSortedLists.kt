d0aadb84c039dbe23a772a22f34a7662c03027fd
Eugene Ivanov <most1360@yandex.ru>
2022-07-11 14:51:59 +0300

    Commit#5

---
+++ src/main/kotlin/leetcode/MergeTwoSortedLists.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+import kotlin.math.max
+
+fun main() {
+    val list = intArrayOf(1, 1, 0, 1, 1, 1)
+    println(findMaxConsecutiveOnes(list))
+}
+
+fun findMaxConsecutiveOnes(nums: IntArray): Int {
+    var counter = 0
+    var max = 0
+    for (el in nums) {
+        if (el == 1) {
+            counter += 1
+            max = max(max, counter)
+        } else counter = 0
+    }
+    return (counter)
+}
